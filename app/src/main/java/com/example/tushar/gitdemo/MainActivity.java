5dfe02d8b68f7bfe9625107ba48231d2db54caa4
Rohit Gupta <grohit00777@gmail.com>
2016-10-16 17:26:25 +0530

    message

--- app/src/main/java/com/example/tushar/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/tushar/gitdemo/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT).show();
+
     }
 }
