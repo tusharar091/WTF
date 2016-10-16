a4d712adc0d821361c075f7c22968684c1f364c9
Rohit Gupta <grohit00777@gmail.com>
2016-10-16 17:48:30 +0530

    File imported

--- app/src/main/java/com/example/tushar/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/tushar/gitdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tushar.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT).show();
+         Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
 
     }
 }
